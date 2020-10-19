7e3b00a738e76dff29dd5ee196c68147d116df84
Brenda-M <brendanjuguna1@gmail.com>
2020-10-19 11:19:46 +0300

    change relative path

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
 javac -cp .;libs\mysql-connector-java-8.0.19.jar -d . *.java
 
-java -cp .;libs\mysql-connector-java-8.0.19.jar tests.Education
+java -cp .;libs\mysql-connector-java-8.0.19.jar tests.Educatio
