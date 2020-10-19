7e8164cdfce6cbf698afc4f13b9c1585ed203bc9
Brenda-M <brendanjuguna1@gmail.com>
2020-10-19 11:18:05 +0300

    first commit

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
-javac -cp .;C:\Jupyter\Eve\mso\libs\mysql-connector-java-8.0.19.jar -d . *.java
+javac -cp .;libs\mysql-connector-java-8.0.19.jar -d . *.java
 
-java -cp .;C:\Jupyter\Eve\mso\libs\mysql-connector-java-8.0.19.jar tests.Education
+java -cp .;libs\mysql-connector-java-8.0.19.jar tests.Education
